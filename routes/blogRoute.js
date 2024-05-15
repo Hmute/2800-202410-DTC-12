4a0af68865ba323c706eb4f563e2ad85b917b1c0
Marc Angelo Arnaldo <arnaldomarcangelo@gmail.com>
2024-05-14 18:18:33 -0700

    Add the route for blog and implement a partial design for blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,2 +1,10 @@
 // This route is only for blog page. Do not touch if you're not Marc. Create your own instead.
 
+const express = require('express');
+const router = express.Router();
+
+router.get('/blog', (req, res) => {
+    res.render('blogPage', {page: 'Blog Posts'});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 /* This is our main server. We import our routes here from the routes folder. 
 This will result in less conflict. Just google or chatGPT how to export routes*/
 
-
 //Richard's script for collapsing meals and exercises button on homepage
 const express = require('express');
 const app = express();
+const path = require('path'); // Import the path module for path.join
 const ejs = require('ejs'); // need to import because it's 'require' (npm i ejs)
+
+const blogRoute = require('./routes/blogRoute');
+
+//############################################Do not touch######################################################
+app.use('/css', express.static(path.join(__dirname, 'css'))); // Need this to access the css files. Do not remove.
+app.use('/images', express.static(path.join(__dirname, 'images'))); // Need this to access the css files. Do not remove.
 app.set('view engine', 'ejs');
+//####################################################################################################
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.use(blogRoute);
 
 
 app.listen(3000, () => {
