33109a566ccf2b5708ee182b706a2e7dac96804d
Richard Li <rli108@my.bcit.ca>
2024-05-23 11:20:14 -0700

    Add updated changes to Routine.js and homeBot features

--- routes/Routine.js
+++ routes/Routine.js
@@ -1,8 +1,35 @@
+// const mongoose = require('mongoose');
+
+// const workoutSchema = new mongoose.Schema({
+//     name: String,
+//     repetitions: Number,
+// });
+
+// const routineSchema = new mongoose.Schema({
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     name: String, // Optional: You can add a name for the routine
+//     exercises: [workoutSchema],
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// module.exports = mongoose.model('Routine', routineSchema);
+
 const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
-    name: String,
-    repetitions: Number,
+    name: { type: String, required: true },
+    repetitions: { type: Number, required: true },
+    sets: { type: Number, default: 0 },
+    weight: { type: Number, default: 0 },
+    time: { type: Number, default: 0 }, // in seconds
+    completion: { type: String, enum: ['Yes', 'No'], default: 'No' },
 });
 
 const routineSchema = new mongoose.Schema({
@@ -19,4 +46,4 @@ const routineSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('Routine', routineSchema);
+module.exports = mongoose.models.Routine || mongoose.model('Routine', routineSchema);
